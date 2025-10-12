e3fa97d774d109fdffdeb1c81e6f44c43261f49a
Juan David Sandoval Salvador <102970701+sandovaldavid@users.noreply.github.com>
2025-10-12 06:06:10 +0000

    ✨ feat(ui): update exports for Avatar and Badge components

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,7 @@
-export { default as Avatar } from './avatar.astro';
-export { default as Badge } from './badge.astro';
+export { Avatar } from './avatar';
+export { Badge } from './badge';
+export { default as LinkButton } from './link-button.astro';
+export { default as LinkInline } from './link-inline.astro';
 export { default as SectionContainer } from './section-container.astro';
+export { SocialPill } from './social-pill';
+export { default as TitleSection } from './title-section.astro';
