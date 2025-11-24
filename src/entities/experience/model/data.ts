92cae966320d99172bcbb190d834288d8d027b66
Juan David Sandoval Salvador <sandovaldavid2201@gmail.com>
2025-11-24 09:24:00 -0500

    ♻️ refactor(experience): remove outdated experience entries

--- src/entities/experience/model/data.ts
+++ src/entities/experience/model/data.ts
@@ -13,28 +13,10 @@ export function getExperienceData(t: (key: string) => string): ExperienceList {
 			company: t('experience.chirasoft.company'),
 		},
 		{
-			date: t('experience.harvardx.date'),
-			title: t('experience.harvardx.title'),
-			description: t('experience.harvardx.description'),
-			company: t('experience.harvardx.company'),
-		},
-		{
-			date: t('experience.data-science.date'),
-			title: t('experience.data-science.title'),
-			description: t('experience.data-science.description'),
-			company: t('experience.data-science.company'),
-		},
-		{
 			date: t('experience.technical-support.date'),
 			title: t('experience.technical-support.title'),
 			description: t('experience.technical-support.description'),
 			company: t('experience.technical-support.company'),
-		},
-		{
-			date: t('experience.alura.date'),
-			title: t('experience.alura.title'),
-			description: t('experience.alura.description'),
-			company: t('experience.alura.company'),
 		},
 	];
 }
